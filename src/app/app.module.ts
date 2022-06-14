e0ddf93409648dd893f2172b81927485944b76ef
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2022-06-14 12:54:14 +0530

    Created navbar and routing links.
    Created navbar and routing links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { WorksComponent } from './works/works.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    WorksComponent,
+    ContactsComponent,
+    ExperienceComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'experience', component: ExperienceComponent },
+      { path: 'works', component: WorksComponent },
+      { path: 'contacts', component: ContactsComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
