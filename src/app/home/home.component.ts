4487cc996f480e82f090803a6d1e173d5b58caba
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2022-06-15 17:26:29 +0530

    Made changes in home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  imagePath: string = '';
+  textToChange: string = '';
+  words: any;
   ngOnInit(): void {
+    this.imagePath = '../../assets/avatar-1.svg';
   }
-
+  constructor() {
+    let words = ['frontend', 'backend'];
+    words.forEach((element) => {
+      return (this.textToChange = element);
+    });
+    this.textToChange = '';
+  }
 }
