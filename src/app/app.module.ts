e59ca8a818c99f42cb00b871a404b2e9a44ee0a2
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2023-04-10 17:37:38 +0530

    Update contact and about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { WorksComponent } from './works/works.component';
@@ -22,14 +21,16 @@ import { EffectsComponent } from './effects/effects.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'about', component: AboutComponent },
-      { path: 'experience', component: ExperienceComponent },
-      { path: 'works', component: WorksComponent },
-      { path: 'contacts', component: ContactsComponent },
-    ]),
+    RouterModule.forRoot(
+      [
+        { path: '', component: HomeComponent },
+        { path: 'about', component: AboutComponent },
+        { path: 'experience', component: ExperienceComponent },
+        { path: 'works', component: WorksComponent },
+        { path: 'contacts', component: ContactsComponent },
+      ],
+      { useHash: true }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent],
