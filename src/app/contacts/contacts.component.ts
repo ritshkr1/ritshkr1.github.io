2f2dfe732157fbd0399933ab460f6038aefc9b4d
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2024-04-25 23:45:22 +0530

    ng build for server

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class ContactsComponent implements OnInit {
 
