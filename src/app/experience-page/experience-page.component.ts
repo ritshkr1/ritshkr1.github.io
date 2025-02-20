4972ee9be0c8938783f7b91ebb950d56f7f0f159
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2025-02-20 12:10:58 +0530

    updated codes

--- src/app/experience-page/experience-page.component.ts
+++ src/app/experience-page/experience-page.component.ts
@@ -14,6 +14,14 @@ export class ExperiencePageComponent {
       duration: 'Apr 2024 - Oct 2024',
       description: 'Led the front-end team, integrated Azure Active Directory (AAD) for secure authentication, and managed authentication tokens for secure client-server communication. Upgraded PrimeNG, implemented Firebase for real-time user notifications, and added filters for multi-row reports.',
       technologies: ['Angular15','TypeScript','JavaScript', 'Azure AD', 'PrimeNG', 'Firebase','Bootstrap', 'Nebular UI','NGX Admin','RxJs','Git','Github','Ngx-Charts','Notfy','Ngx-toastr','MSAL'],
+      projects: [
+        {
+          name: "MUR Shipping",
+          description: "A scalable system for managing shipping and maintaining ships to load various resources.",
+          technologies: ["Angular15", ".Net", "AWS", "Azure Auth -MSAL", "Primeng", "NGX-Admin", "Nebular"],
+        },
+     
+      ]
     },
     {
       title: 'Angular Developer',
@@ -22,6 +30,14 @@ export class ExperiencePageComponent {
       duration: 'Apr 2022 - Feb 2024',
       description: 'Implemented and maintained product-based applications using Angular 12, AG Grid, and TailwindCSS. Collaborated with senior developers and QA teams to deliver high-quality results, optimized code, and integrated frontend-backend functionalities.',
       technologies: ['Angular 12', 'TypeScript','JavaScript', 'AG Grid', 'TailwindCSS', 'Bootstrap','Angular Material','Gitlab','Sourcetree', 'Git','Playwright',],
+      projects: [
+        {
+          name: "School Management Software",
+          description: "Advanta Rapid ERP is the only one software that you need to manage all aspects of your school and colleges.",
+          technologies: ["Angular15", "Node.js", "AWS", "Github","Rxjs"],
+        },
+        
+      ]
     },
     {
       title: 'Angular Trainee',
@@ -30,6 +46,7 @@ export class ExperiencePageComponent {
       duration: 'Jan 2022 - Mar 2022',
       description: 'Gained practical experience in front-end development by assisting with the implementation of Angular-based web applications. Participated in code reviews, troubleshooting, and debugging.',
       technologies: ['Angular','TypeScript','JavaScript','Bootstrap','Angular Material','Git','Github','Npm'],
+      projects: null
     }
   ];
 
